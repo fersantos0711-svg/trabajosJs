15fc698a2e714b8439755f8f96b8e88e8cf7b60d
fersantos0711-svg <fersantos0711@gmail.com>
2026-01-07 20:45:12 -0600

    ej 1

---
+++ ejercicio1.js
@@ -0,0 +1,17 @@
+// Ejercicio 1 - Declarar dos variables: Nuestra tarea será declarar dos variables: una 
+//con el nombre edad y la otra con el nombre peso. Después deberás asignarle un 
+//valor. 
+//Pista: Recordemos que para crear una variable debemos utilizar la palabra 
+//reservada "let", seguida del nombre con el cual luego le haremos referencia.
+ 
+const prompt = require("prompt-sync")();
+
+
+
+let edad = 33;
+let peso = 85;
+
+console.log("Edad:", edad);
+console.log("Peso:", peso);
+// En este ejemplo, hemos declarado dos variables: "edad" con un valor de 33 y
+// "peso" con un valor de 85. Luego, imprimimos sus valores en la consola.
